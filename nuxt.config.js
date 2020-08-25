3fbba01c84c60e03334d10843103cbf89c3f9dba
Zhanadil1509 <zhanadil1509@gmail.com>
2020-08-25 14:56:33 +0600

    add generate nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,12 @@ export default {
   ** See https://nuxtjs.org/api/configuration-mode
   */
   mode: 'universal',
+
+  generate: {
+    routes: [
+      ''
+    ]
+  },
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
