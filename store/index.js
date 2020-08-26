4fc37df0a516db40d98b32a7b5e0080ec659d3a4
Zhanadil1509 <zhanadil1509@gmail.com>
2020-08-26 18:51:30 +0600

    add CategorySlug, actions on CategorySlug

--- store/index.js
+++ store/index.js
@@ -1,43 +1,91 @@
 const sleep = m => new Promise(r => setTimeout(r, m))
 const categories = [
   {
+    id: 'cats',
+    cTitle: 'Котики',
     cName: 'Котики',
     cSlug: 'cats',
-    cImage: 'https://source.unsplash.com/300x300/?cat,cats'
+    cMetaDescription: 'Мета описание',
+    cDesc: 'Описание',
+    cImage: 'https://source.unsplash.com/300x300/?cat,cats',
+    products: []
   },
   {
+    id: 'dogs',
+    cTitle: 'Собачки',
     cName: 'Собачки',
     cSlug: 'dogs',
-    cImage: 'https://source.unsplash.com/300x300/?dog,dogs'
+    cMetaDescription: 'Мета описание',
+    cDesc: 'Описание',
+    cImage: 'https://source.unsplash.com/300x300/?dog,dogs',
+    products: []
   },
   {
+    id: 'wolfs',
+    cTitle: 'Волчки',
     cName: 'Волчки',
     cSlug: 'wolfs',
-    cImage: 'https://source.unsplash.com/300x300/?wolf'
+    cMetaDescription: 'Мета описание',
+    cDesc: 'Описание',
+    cImage: 'https://source.unsplash.com/300x300/?wolf',
+    products: []
   },
   {
+    id: 'bulls',
+    cTitle: 'Бычки',
     cName: 'Бычки',
     cSlug: 'bulls',
-    cImage: 'https://source.unsplash.com/300x300/?ox'
+    cMetaDescription: 'Мета описание',
+    cDesc: 'Описание',
+    cImage: 'https://source.unsplash.com/300x300/?bull',
+    products: []
   }
 ]
 
+const addProductsToCategory = (products, category) => {
+  const categoryInner = { ...category, products: [] }
+  products.map(p => {
+    if(p.category_id === category.id) {
+      categoryInner.products.push({
+        id: p.id,
+        pName: p.pName,
+        pSlug: p.pSlug,
+        pPrice: p.pPrice,
+        image: `https://source.unsplash.com/300x300/?${p.pName}`
+      })
+    }
+  })
+  return categoryInner
+}
+
 export const state = () => ({
-  categoriesList: []
+  categoriesList: [],
+  currentCategory: {},
+  currentProduct: {}
 })
 export const mutations = {
   SET_CATEGORIES_LIST (state, categories) {
     state.categoriesList = categories
+  },
+  SET_CURRENT_CATEGORY (state, category) {
+    state.currentCategory = category
   }
 }
 export const actions = {
   async getCategoriesList ({ commit }) {
     try {
-      await sleep(1000)
+      await sleep(500)
       await commit('SET_CATEGORIES_LIST', categories)
     } catch (err) {
       console.log(err)
       throw new Error('Внутреняя ошибка сервера, сообщите администратору')
     }
+  },
+  async getCurrentCategory ({commit}, {route}) {
+    await sleep(500)
+    const category = categories.find((cat) => cat.cSlug === route.params.CategorySlug)
+    const products = await this.$axios.$get('/mock/products.json')
+
+    await commit('SET_CURRENT_CATEGORY', addProductsToCategory(products, category))
   }
 }
